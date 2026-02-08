d37c8da86b8c78078a66c6ce1bc947354fd17853
SamuelRaj-tech <samuelrajk76@gmail.com>
2026-02-08 18:12:43 +0530

    backend server.js cors code change

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,11 @@ dotenv.config();
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+    origin: process.env.FRONTEND_URL || "http://localhost:3000",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  }));
 app.use(express.json());
 
 app.get('/test', (req, res) => {
